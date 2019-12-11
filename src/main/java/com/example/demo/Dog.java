2c979b2a610cbc7e9dcaa631a98b1f4e6663d6e4
johnsuh01 <johnsuh01@hotmail.com>
2019-12-11 14:02:46 -0500

    SpringBoot_301 Exercise

---
+++ src/main/java/com/example/demo/Dog.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+
+@Entity
+public class Dog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+
+    @NotNull
+    @Min(1)
+    private long age;
+}
